a1a030287f404e78a61122ca884a2f55f9c177ba
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2023-03-24 17:23:32 +0300

    HW

--- src/main/java/ru/itsjava/services/ClientServiceImpl.java
+++ src/main/java/ru/itsjava/services/ClientServiceImpl.java
@@ -1,14 +1,19 @@
 package ru.itsjava.services;
 
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j;
 
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+@Log4j
 public class ClientServiceImpl implements ClientService {
     public final static int PORT = 8081;
     public final static String HOST = "localhost";
+    public MessageInputService messageInputService;
+    public PrintWriter serverWriter;
+    public Scanner scanner;
 
     @SneakyThrows
     @Override
@@ -16,42 +21,54 @@ public class ClientServiceImpl implements ClientService {
         Socket socket = new Socket(HOST, PORT);
         if (socket.isConnected()) {
             new Thread(new SocketRunnable(socket)).start();
+            log.info("Client start");
+            serverWriter = new PrintWriter(socket.getOutputStream());
+            messageInputService = new MessageInputServiceImpl(System.in);
+            scanner = new Scanner(System.in);
+            authorizationOrRegistration();
 
-            PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
-            MessageInputService messageInputService =
-                    new MessageInputServiceImpl(System.in);
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("1) Авторизиция ");
-            System.out.println("2) Регистрация ");
-            int menu = scanner.nextInt();
-            if (menu == 1) {
-                System.out.println("Введите свой логин");
-                String login = messageInputService.getMessage();
-
-                System.out.println("Введите свой пароль");
-                String password = messageInputService.getMessage();
-
-                serverWriter.println("!autho!" + login + ":" + password);
-                serverWriter.flush();
-
-            } else if (menu == 2) {
-                System.out.println("Введите свой логин");
-                String login = messageInputService.getMessage();
-
-                System.out.println("Введите свой пароль");
-                String password = messageInputService.getMessage();
-
-                serverWriter.println("!regis!" + login + ":" + password);
-                serverWriter.flush();
-            }
             while (true) {
                 String consoleMessage = messageInputService.getMessage();
                 if (!consoleMessage.equals("Exit")) {
-                    serverWriter.println(consoleMessage);
+                    if (consoleMessage.isEmpty()) {
+                        System.out.println("Сообщение не должно быть пустым");
+                    } else {
+                        serverWriter.println(consoleMessage.trim());
+                        serverWriter.flush();
+                    }
+                } else {
+                    serverWriter.println("Client disconnected");
                     serverWriter.flush();
-                } else System.exit(0);
+                    log.info("Client disconnected");
+                    System.exit(0);
+                }
             }
         }
     }
+
+    public void authorizationOrRegistration() {
+        System.out.println("1) Авторизиция ");
+        System.out.println("2) Регистрация ");
+
+        int menu = scanner.nextInt();
+        String prefix = null;
+        if (menu == 1) {
+            prefix = "!autho!";
+            log.info("Authorization request");
+        } else if (menu == 2) {
+            prefix = "!regis!";
+            log.info("Registration request");
+        }
+        System.out.println("Введите свой логин");
+        String login = messageInputService.getMessage();
+
+        System.out.println("Введите свой пароль");
+        String password = messageInputService.getMessage();
+
+        serverWriter.println(prefix + login + ":" + password);
+        serverWriter.flush();
+    }
 }
+
+
 
--- src/main/java/ru/itsjava/services/SocketRunnable.java
+++ src/main/java/ru/itsjava/services/SocketRunnable.java
@@ -1,10 +1,13 @@
 package ru.itsjava.services;
 
+import lombok.Getter;
+
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 
 import java.net.Socket;
 
+@Getter
 @RequiredArgsConstructor
 public class SocketRunnable implements Runnable {
     private final Socket socket;
@@ -14,7 +17,6 @@ public class SocketRunnable implements Runnable {
     public void run() {
         MessageInputService serverReader =
                 new MessageInputServiceImpl(socket.getInputStream());
-
         while (true) {
             System.out.println(serverReader.getMessage());
         }
